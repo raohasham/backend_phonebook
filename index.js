c3d683cfcad2678575177059dd6451e9b1beb0f1
raohasham <hashamrao2005@gmail.com>
2024-09-14 14:43:21 +0500

    initial upload

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const app = express()
+
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+
+})
+
+app.get('/api/info',(req,res)=>{
+    const date = new Date().toLocaleDateString()
+    
+   const contacts = persons.length
+   res.send(`this phoonebook has ${contacts} contacts 
+    ${date}
+    `)
+   
+})
+
+app.get('/api/persons/:id',(req,res)=>{
+   const id=req.params.id;
+   const person=persons.find(person=>person.id===id)
+   if(person){
+res.json(person)}
+else{
+    res.status(404).end()
+}
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+    const id = req.params.id
+    persons=persons.filter(person=>person.id!==id)
+    res.status(204).end()
+})
+
+
+const port= 3001
+app.listen(port,()=>{
+    console.log(`app is listening at port ${port}`);
+    
+})
